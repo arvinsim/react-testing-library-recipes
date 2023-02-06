5b0e9236e26448b5163e1a5a35b42b16a74c7b87
Arthur Vincent Simon <developarvin@gmail.com>
2023-02-06 18:06:37 +0800

    Partial commit with errors

--- __tests__/components/ConditionalMock.test.tsx
+++ __tests__/components/ConditionalMock.test.tsx
@@ -1,12 +1,69 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import ConditionalMock from '../../components/ConditionalMock'
+import ConditionalMock, { getConfigFlag } from '../../components/ConditionalMock'
 
 describe('ConditionalMock', () => {
-    it('should show page message if configuration is set to true', () => {
+    test.each([
+        {
+            userName: 'John' 
+        },
+        {
+            userName: 'Jane' 
+        }
+    ])(`show page if config is set to true and $userName is the user`, ({ userName }) => {
+        ;(getConfigFlag as jest.Mock).mockImplementation(
+            (name: string, defaultValue: string) => {
+              const { getConfigFlag: actualImplementation } = jest.requireActual(
+                '../../components/ConditionalMock',
+              )
+              if (name === 'shouldShowPage') {
+                return true
+              if (name === 'userName') {
+                return userName
+              } else {
+                return actualImplementation(name, defaultValue)
+              }
+              }
+            }
+          )
+
         render(
-        <ConditionalMock />
+            <ConditionalMock />
         )
         expect(screen.getByText(/page is shown/i)).toBeInTheDocument()
+        expect(screen.getByText(userName).toBeInTheDocument()
+    })
+
+    test.each([
+        {
+            shouldShowPage: false,
+            userName: 'John' 
+        },
+        {
+            shouldShowPage: false,
+            userName: 'Jane' 
+        },
+    ])(`don't show page if config is set to true and $userName is the user`, (shouldShowPage, userName) => {
+        ;(getConfigFlag as jest.Mock).mockImplementation(
+            (name: string, defaultValue: string) => {
+              const { getConfigFlag: actualImplementation } = jest.requireActual(
+                '../../components/ConditionalMock',
+              )
+              if (name === 'shouldShowPage') {
+                return true
+              if (name === 'userName') {
+                return userName
+              } else {
+                return actualImplementation(name, defaultValue)
+              }
+              }
+            }
+          )
+
+        render(
+            <ConditionalMock />
+        )
+        expect(screen.queryByText(/page is shown/i)).toBeNull()
+        expect(screen.queryByText(userName).toBeNull())
     })
 })
--- components/ConditionalMock.tsx
+++ components/ConditionalMock.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 
 export function getConfigFlag(name: string, defaultValue?: string) {
-    if (name === 'shouldShowPage') {
-        return true
+    switch(name) {
+        case 'shouldShowPage': 
+            return Math.random() > 0.5 ? true : false           
+        case 'userName':
+            return Math.random() > 0.5 ? 'John' : 'Jane'           
+        default:
+            return null
     }
-    return false
 }
-
 
 function ConditionalMock() {
     const shouldShowPage = getConfigFlag('shouldShowPage')
+    const userName = getConfigFlag('userName')
 
     return (<div>
-        <h1>Conditional Mock Configurations</h1>
-        {shouldShowPage ? <span>Page is shown</span> : null}
+        <h1>Unpredictable Configurations</h1>
+        {shouldShowPage ? <span>Page is shown by {userName}</span> : null}
     </div>)
 }
 
