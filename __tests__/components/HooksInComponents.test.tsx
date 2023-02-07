8bd4afac9120911a9cf27e97aac0913c8242018f
Arthur Vincent Simon <developarvin@gmail.com>
2023-02-07 15:29:26 +0800

    Fix test issue with HooksInComponents

--- __tests__/components/HooksInComponents.test.tsx
+++ __tests__/components/HooksInComponents.test.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import HooksInComponents from '../../components/HooksInComponents'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import HooksInComponents from "../../components/HooksInComponents";
+import useGetCars from "../../utils/useGetCars";
 
-jest.mock('../../utils/useGetCars')
+jest.mock("../../utils/useGetCars");
 
-describe('HooksInComponents', () => {
-    it('shows loading page while loading', () => {
-        jest.mock('../../utils/useGetCars', () => {
-            return jest.fn(() => [true, undefined])
-        })
+describe("HooksInComponents", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-       render(
-            <HooksInComponents />
-       ) 
-       expect(screen.getByText(/page is not yet loaded/i)).toBeInTheDocument()
-    })
+  it("shows loading page while loading", () => {
+    (useGetCars as jest.Mock).mockImplementation(() => [true, []]);
 
-    it('shows cars data after loading', () => {
-        jest.mock('../../utils/useGetCars', () => {
-            return jest.fn(() => [false, [{ id: 'm', name: 'Maserati'}, {id: 'b', name: 'Bughatti'}]])
-        })
-       render(
-            <HooksInComponents />
-       ) 
-       expect(screen.getByText(/maserati/i)).toBeInTheDocument()
-       expect(screen.getByText(/bughatti/i)).toBeInTheDocument()
-    })
-})
+    render(<HooksInComponents />);
+    expect(screen.getByText(/page is not yet loaded/i)).toBeInTheDocument();
+  });
+
+  it("shows cars data after loading", () => {
+    (useGetCars as jest.Mock).mockImplementation(() => [
+      false,
+      [
+        { id: "m", name: "Maserati" },
+        { id: "b", name: "Bughatti" },
+      ],
+    ]);
+
+    render(<HooksInComponents />);
+    expect(screen.getByText(/maserati/i)).toBeInTheDocument();
+    expect(screen.getByText(/bughatti/i)).toBeInTheDocument();
+  });
+});
