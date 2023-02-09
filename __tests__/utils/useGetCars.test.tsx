71cb1176558db7c6bac9e5f8afae8348a3ed6562
Arthur Vincent Simon <developarvin@gmail.com>
2023-02-09 11:26:26 +0800

    Add tests that tests for the React hook directly

--- __tests__/utils/useGetCars.test.tsx
+++ __tests__/utils/useGetCars.test.tsx
@@ -1,9 +1,20 @@
 import useGetCars from "../../utils/useGetCars";
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 
 describe("useGetCars", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
   it("returns a Lamborghini and Ferrari", () => {
     const { result } = renderHook(() => useGetCars());
+    act(() => {
+      jest.runAllTimers();
+    });
     expect(result.current).toEqual([
       false,
       [
--- utils/useGetCars.ts
+++ utils/useGetCars.ts
@@ -1,24 +1,30 @@
 import { useEffect, useState } from "react";
 
 type Cars = {
-    id: string
-    name: string
-}
+  id: string;
+  name: string;
+};
 
 function useGetCars(): [boolean, Array<Cars>] {
-    const [cars, setCars] = useState<Array<Cars>>([])
-    const [loading, setLoading] = useState<boolean>(false)
+  const [cars, setCars] = useState<Array<Cars>>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
-    useEffect(() => {
-        setLoading(true)
-        setCars([
-            {id: "l", name: "Lamborghini"},
-            { id: "f", name: "Ferrari"},
-        ])
-        setLoading(false)
-    }, [])
+  useEffect(() => {
+    setLoading(true);
+    const myTimeout = setTimeout(() => {
+      setCars([
+        { id: "l", name: "Lamborghini" },
+        { id: "f", name: "Ferrari" },
+      ]);
+      setLoading(false);
+    }, 3000);
 
-    return [loading, cars]
+    return () => {
+      clearTimeout(myTimeout);
+    };
+  }, []);
+
+  return [loading, cars];
 }
 
-export default useGetCars
+export default useGetCars;
